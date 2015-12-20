ed6e6978a1c6a540ae559ae79a8c271fbaa212ad
Mark Diekhans <markd@soe.ucsc.edu>
2015-12-20 13:33:53 -0800

    got pair-end working

--- lib/gencode_icedb/readsOps.py
+++ lib/gencode_icedb/readsOps.py
@@ -3,6 +3,11 @@ import os
 from pycbio.sys import PycbioException
 from pycbio.sys import fileOps
 
+def isFastq(readsFile):
+    """does file appear to be a fastq by naming convention"""
+    base = fileOps.compressBaseName(readsFile)
+    return base.endswith(".fastq")
+
 def getReadsCatCommand(readsFile):
     """Get command to cat reads file based on extension from either a bam or a fastq, possibly compressed"""
     base = fileOps.compressBaseName(readsFile)
