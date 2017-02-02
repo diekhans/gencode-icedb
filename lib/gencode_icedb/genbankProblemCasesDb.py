f077177288c7a5ba04079474c52070a23eaefaa3
Mark Diekhans <markd@soe.ucsc.edu>
2017-02-01 20:29:32 -0500

    forgotten commit

---
+++ lib/gencode_icedb/genbankProblemCasesDb.py
@@ -0,0 +1,49 @@
+"""
+GenBank problem case accession and reason table.
+"""
+from collections import namedtuple
+from pycbio.hgdata.hgLite import HgLiteTable
+
+
+class GenbankProblemCase(namedtuple("GenbankProblemCase",
+                                    ("acc", "reason"))):
+    """a problem case"""
+    __slots__ = ()
+    pass
+
+
+class GenbankProblemCaseDbTable(HgLiteTable):
+    """
+    Storage for problem cases
+    """
+    __createSql = """CREATE TABLE {table} (
+            acc text not null,
+            reason text not null);"""
+    __insertSql = """INSERT INTO {table} (acc, reason) VALUES (?, ?);"""
+    __indexSql = """CREATE UNIQUE INDEX {table}_acc on {table} (acc);"""
+
+    def __init__(self, conn, table, create=False):
+        super(GenbankProblemCaseDbTable, self).__init__(conn, table)
+        if create:
+            self.create()
+
+    def create(self):
+        """create table"""
+        self._create(self.__createSql)
+
+    def index(self):
+        """create index after loading"""
+        self._index(self.__indexSql)
+
+    def loads(self, rows):
+        """load rows into table.  Each element of row is a list, tuple, or GenbankProblemCase object of name and seq"""
+        self._inserts(self.__insertSql, rows)
+
+    def get(self, acc):
+        "retrieve a record by accession, or None"
+        sql = "SELECT acc, reason FROM {table} WHERE acc = ?;"
+        row = next(self.query(sql, acc), None)
+        if row is None:
+            return None
+        else:
+            return GenbankProblemCase(*row)
