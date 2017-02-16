a019dbd3c9fb4fa5643e2eed062cfc99850d117d
Mark Diekhans <markd@soe.ucsc.edu>
2017-02-16 14:06:56 -0800

    mock seqreader tests

--- lib/gencode_icedb/tsl/evidFeatures.py
+++ lib/gencode_icedb/tsl/evidFeatures.py
@@ -2,12 +2,32 @@
 Convert PSL to features, closing (and tracking gaps)
 """
 from __future__ import print_function
+from pycbio.hgdata import dnaOps
 
 
-class SeqRegion(object):
-    def __init__(self, twoBitReader, chrom, start, end):
-        self.chrom, self.start, self.end = chrom, start, end
-        self.twoBitSeq = twoBitReader[chrom]
+class SeqReader(object):
+    """
+    Reads sequences from twoBitReader, which can be substituted with a mock
+    version.
+    """
+    def __init__(self, twoBitReader):
+        self.twoBitReader = twoBitReader
+        self.chrom = self.size = None
+
+    def __obtainChrom(self, chrom):
+        if self.chrom != chrom:
+            self.twoBitSeq = self.twoBitReader[chrom]
+            self.chrom = chrom
+            self.size = len(self.twoBitSeq)
+
+    def get(self, chrom, start, end, strand=None):
+        self.__obtainChrom(chrom)
+        if strand == '-':
+            start, end = self.size - end, self.size - start
+        seq = self.twoBitSeq[start:end]
+        if strand == '-':
+            seq = dnaOps.reverseComplement(seq)
+        return seq
 
 
 class EvidFeature(object):
@@ -25,18 +45,20 @@ class EvidExon(EvidFeature):
         self.qInsertBases, self.tInsertBases = qInsertBases, tInsertBases
 
     def __str__(self):
-        return "exon {}-{} qIns={} tIns={}".format(self.start, self.end, self.qInsertBases, self.tInsertBases)
+        return "exon {}-{} qIns={} tIns={} ".format(self.start, self.end,
+                                                    self.qInsertBases, self.tInsertBases)
 
 
 class EvidIntron(EvidFeature):
-    __slots__ = ("qDeleteBases", )
+    __slots__ = ("qDeleteBases", "startBases", "endBases")
 
-    def __init__(self, start, end, qDeleteBases):
+    def __init__(self, start, end, qDeleteBases, startBases, endBases):
         super(EvidIntron, self).__init__(start, end)
-        self.qDeleteBases = qDeleteBases
+        self.qDeleteBases, self.startBases, self.endBases = qDeleteBases, startBases, endBases
 
     def __str__(self):
-        return "intron {}-{} qDel={}".format(self.start, self.end, self.qDeleteBases)
+        return "intron {}-{} qDel={} sjBases={}...{}".format(self.start,
+                                                             self.end, self.qDeleteBases, self.startBases, self.endBases)
 
 
 class EvidFeatures(list):
@@ -54,20 +76,20 @@ class EvidFeatures(list):
         self.qStart = psl.qStart
         self.qEnd = psl.qEnd
         self.qSize = psl.qSize
-        self.__buildFeatures(psl)
+        self.__buildFeatures(psl, seqRegion)
 
     def __str__(self):
         return "t={} {}, q={}:{}={} {}".format(self.chrom, self.strand,
                                                self.qName, self.qStart,
                                                self.qEnd, self.qSize)
 
-    def __buildFeatures(self, psl):
+    def __buildFeatures(self, psl, seqRegion):
         iBlkStart = 0
         while iBlkStart < len(psl.blocks):
             iBlkEnd = self.__findExonEnd(psl, iBlkStart)
             self.__makeExon(psl, iBlkStart, iBlkEnd)
             if iBlkEnd < len(psl.blocks):
-                self.__makeIntron(psl, iBlkEnd)
+                self.__makeIntron(psl, iBlkEnd, seqRegion)
             iBlkStart = iBlkEnd
 
     def __tGapSize(self, psl, iBlkStart, iBlkEnd):
@@ -89,8 +111,12 @@ class EvidFeatures(list):
                              psl.blocks[iBlkEnd - 1].tEnd,
                              qInsertBases, tInsertBases))
 
-    def __makeIntron(self, psl, iBlkNext):
+    def __makeIntron(self, psl, iBlkNext, seqRegion):
         qDeleteBases = psl.blocks[iBlkNext].qStart - psl.blocks[iBlkNext - 1].qEnd
+        startBases = seqRegion.get(psl.tName, psl.blocks[iBlkNext].tStart,
+                                   psl.blocks[iBlkNext].tStart + 2, psl.getTStrand())
+        endBases = seqRegion.get(psl.tName, psl.blocks[iBlkNext].tEnd - 2,
+                                 psl.blocks[iBlkNext].tEnd, psl.getTStrand())
         self.append(EvidIntron(psl.blocks[iBlkNext - 1].tEnd,
                                psl.blocks[iBlkNext].tStart,
-                               qDeleteBases))
+                               qDeleteBases, startBases, endBases))
--- tests/tsl/classify/classifyUnitTests.py
+++ tests/tsl/classify/classifyUnitTests.py
@@ -8,11 +8,70 @@ if __name__ == '__main__':
 import unittest
 from pycbio.sys.testCaseBase import TestCaseBase
 from pycbio.hgdata.psl import PslTbl
-from gencode_icedb.tsl.evidFeatures import EvidFeatures
+from pycbio.tsv import TsvReader
+from pycbio.sys import fileOps
+from gencode_icedb.tsl.evidFeatures import SeqReader, EvidFeatures
+from twobitreader import TwoBitFile
+
+
+twoBitHg19 = "/hive/data/genomes/hg19/hg19.2bit"
+mockReaderHg19Tsv = "mockReaderHg19.tsv"
+updateMockReader = False   # set this to update from a real run
+
+
+class MockSeqReader(object):
+    """Fake SeqReader when 2bit isn't there"""
+    def __init__(self, mockDataTsv):
+        self.mockData = dict()
+        for row in TsvReader(mockDataTsv,
+                             typeMap={"start": int,
+                                      "end": int}):
+            self.mockData[tuple(row.chrom, row.start, row.end, row.strand)] = row.seq
+
+    def get(self, chrom, start, end, strand=None):
+        if strand is None:
+            strand = '+'
+        return self.mockData[(chrom, start, end, strand)]
+
+
+class MockSeqWriter(object):
+    """Wrapper around SeqReader that create mock data from twoBit """
+    def __init__(self, seqReader, mockDataTsv):
+        self.seqReader = seqReader
+        self.mockDataFh = open(mockDataTsv, "w")
+        fileOps.prRowv(self.mockDataFh, "chrom", "start", "end", "seq")
+
+    def get(self, chrom, start, end, strand=None):
+        if strand is None:
+            strand = '+'
+        seq = self.seqReader.get(chrom, start, end, strand)
+        fileOps.prRowv(self.mockDataFh, chrom, start, end, seq)
+        return seq
 
 
 class EvidenceTests(TestCaseBase):
+    seqReader = None
     set1Psls = None
+
+    def __getRealSeqReader(self):
+        seqReader = SeqReader(TwoBitFile(twoBitHg19))
+        if updateMockReader:
+            seqReader = MockSeqWriter(seqReader,
+                                      self.getInputFile(mockReaderHg19Tsv))
+        return seqReader
+
+    def __getMockSeqReader(self):
+        if updateMockReader:
+            raise Exception("updateMockReader is True, however twobit {} is not available".format(twoBitHg19))
+        return MockSeqReader(self.getInputFile(mockReaderHg19Tsv))
+
+    def __obtainSeqReader(self):
+        if self.seqReader is None:
+            if os.path.exists(twoBitHg19):
+                self.seqReader = self.__getRealSeqReader()
+            else:
+                self.seqReader = self.__getMockSeqReader()
+        return self.seqReader
 
     def __requireSet1Psls(self):
         if self.set1Psls is None:
@@ -26,57 +85,58 @@ class EvidenceTests(TestCaseBase):
     def __assertFeatures(self, feats, expectFeatsStr, expectFeatStrs):
         self.assertEqual(str(feats), expectFeatsStr)
         self.assertEqual([str(f) for f in feats], expectFeatStrs)
-    
+
     def testAF010310(self):
         psl = self.__getSet1Psl("AF010310.1")
-        feats = EvidFeatures(psl, '-', None)
+        feats = EvidFeatures(psl, '-', self.__obtainSeqReader())
         self.__assertFeatures(feats, "t=18900294 -, q=AF010310.1:0=888 901",
-                              ['exon 32398640-32398738 qIns=1 tIns=4',
-                               'intron 32398738-32400065 qDel=0',
-                               'exon 32400065-32400164 qIns=0 tIns=0',
-                               'intron 32400164-32403527 qDel=0',
-                               'exon 32403527-32403616 qIns=0 tIns=0',
-                               'intron 32403616-32403691 qDel=0',
-                               'exon 32403691-32403763 qIns=1 tIns=0',
-                               'intron 32403763-32403763 qDel=1',
-                               'exon 32403763-32403897 qIns=2 tIns=0',
-                               'intron 32403897-32403897 qDel=1',
-                               'exon 32403897-32403932 qIns=3 tIns=0',
-                               'intron 32403932-32403938 qDel=10',
-                               'exon 32403938-32403987 qIns=6 tIns=1',
-                               'intron 32403987-32403987 qDel=1',
-                               'exon 32403987-32404031 qIns=3 tIns=0',
-                               'intron 32404031-32404031 qDel=1',
-                               'exon 32404031-32404272 qIns=3 tIns=1'])
-
+                              ['exon 32398640-32398738 qIns=1 tIns=4 ',
+                               'intron 32398738-32400065 qDel=0 sjBases=GT...AG',
+                               'exon 32400065-32400164 qIns=0 tIns=0 ',
+                               'intron 32400164-32403527 qDel=0 sjBases=GA...GG',
+                               'exon 32403527-32403616 qIns=0 tIns=0 ',
+                               'intron 32403616-32403691 qDel=0 sjBases=GC...CA',
+                               'exon 32403691-32403763 qIns=1 tIns=0 ',
+                               'intron 32403763-32403763 qDel=1 sjBases=GC...GA',
+                               'exon 32403763-32403897 qIns=2 tIns=0 ',
+                               'intron 32403897-32403897 qDel=1 sjBases=TA...CC',
+                               'exon 32403897-32403932 qIns=3 tIns=0 ',
+                               'intron 32403932-32403938 qDel=10 sjBases=AG...CA',
+                               'exon 32403938-32403987 qIns=6 tIns=1 ',
+                               'intron 32403987-32403987 qDel=1 sjBases=AC...AC',
+                               'exon 32403987-32404031 qIns=3 tIns=0 ',
+                               'intron 32404031-32404031 qDel=1 sjBases=GC...TC',
+                               'exon 32404031-32404272 qIns=3 tIns=1 '])
 
     def testX96484(self):
         psl = self.__getSet1Psl("X96484.1")
-        feats = EvidFeatures(psl, '+', None)
+        feats = EvidFeatures(psl, '+', self.__obtainSeqReader())
         self.__assertFeatures(feats, "t=18893922 +, q=X96484.1:48=1067 1080",
-                              ['exon 18893922-18893997 qIns=0 tIns=0',
-                               'intron 18893997-18894077 qDel=0',
-                               'exon 18894077-18894238 qIns=1 tIns=0',
-                               'intron 18894238-18897684 qDel=0',
-                               'exon 18897684-18897785 qIns=0 tIns=0',
-                               'intron 18897785-18898400 qDel=0',
-                               'exon 18898400-18898541 qIns=0 tIns=0',
-                               'intron 18898541-18899052 qDel=0',
-                               'exon 18899052-18899592 qIns=0 tIns=0'])
-        
+                              ['exon 18893922-18893997 qIns=0 tIns=0 ',
+                               'intron 18893997-18894077 qDel=0 sjBases=CT...GA',
+                               'exon 18894077-18894238 qIns=1 tIns=0 ',
+                               'intron 18894238-18897684 qDel=0 sjBases=TG...AG',
+                               'exon 18897684-18897785 qIns=0 tIns=0 ',
+                               'intron 18897785-18898400 qDel=0 sjBases=GC...AG',
+                               'exon 18898400-18898541 qIns=0 tIns=0 ',
+                               'intron 18898541-18899052 qDel=0 sjBases=GA...TG',
+                               'exon 18899052-18899592 qIns=0 tIns=0 '])
+
     def testSet1(self):
         # just run through to see if they all convert
         self.__requireSet1Psls()
         cnt = 0
         for psl in self.set1Psls:
-            EvidFeatures(psl, psl.getTStrand(), None)
+            EvidFeatures(psl, psl.getTStrand(), self.__obtainSeqReader())
             cnt += 1
         self.assertEqual(cnt, 81)
+
 
 def suite():
     ts = unittest.TestSuite()
     ts.addTest(unittest.makeSuite(EvidenceTests))
     return ts
+
 
 if __name__ == '__main__':
     unittest.main()
