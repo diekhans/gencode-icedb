adf4a27911e14be848ad45d4deb6521a1fbba96c
Mark Diekhans <markd@soe.ucsc.edu>
2017-08-28 13:49:59 -0700

    fixed gene stats

--- lib/gencode_icedb/rsl/supportAnalysis.py
+++ lib/gencode_icedb/rsl/supportAnalysis.py
@@ -3,7 +3,7 @@ Common functions to support analysis.
 """
 
 from gencode_icedb.rsl.rslModel import GencodeSupport, GencodeNovel
-
+from pycbio.hgbrowser.coords import Coords
 
 def isPseudo(suppRec):
     # FIXME: switch to using functions now in CCDS tree
@@ -21,12 +21,18 @@ def _intronSupportReaderFilter(isNovelDb, rec):
 
 def intronSupportReader(isNovelDb, chrom=None):
     """read with filtering introns support from database.  Should have already
-    connected and bound ORM.  If chrom is supplied, only query that chrom for
-    testing purposes."""
+    connected and bound ORM.  If chrom is supplied, it can be a chrom or chrom range. Only query
+    that chrom for testing purposes."""
     ormCls = GencodeNovel if isNovelDb else GencodeSupport
     query = ormCls.select()
     if chrom is not None:
-        query = query.where(ormCls.chrom == chrom)
+        if chrom.find(':') >= 0:
+            assert False, "need to add bin to make this not crawl"
+            # FIXME: chould use bin
+            coords = Coords.parse(chrom)
+            query = query.where(ormCls.chrom == coords.chrom and ormCls.intronStart < coords.end and ormCls.intronEnd > coords.start)
+        else:
+            query = query.where(ormCls.chrom == chrom)
     for rec in query:
         if _intronSupportReaderFilter(isNovelDb, rec):
             yield rec
