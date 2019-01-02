8e2afc72647bbe4a7526fecb32b731301e1c2be2
Mark Diekhans <markd@soe.ucsc.edu>
2019-01-01 19:54:09 -0500

    removed unused code

--- lib/gencode_icedb/rsl/gencodeIntronEvid.py
+++ lib/gencode_icedb/rsl/gencodeIntronEvid.py
@@ -29,62 +29,6 @@ def intronEvidSupportLevel(numReads):
     else:
         return IntronSupportLevel.NONE
 
-
-# FIXME: no longer needed
-
-class GencodeIntronEvidReader(tsv.TsvReader):
-    "TSV reader for evidence"
-
-    # chrom intronStart intronEnd novel annotStrand rnaSeqStrand intronMotif
-    # numUniqueMapReads numMultiMapReads transcripts
-
-    spliceJunctionTsvTypeMap = {
-        "intronStart": int,
-        "intronEnd": int,
-        "novel": lambda v: bool(int(v)),
-        "annotStrand": tsv.strOrNoneType,
-        "rnaSeqStrand": tsv.strOrNoneType,
-        "numUniqueMapReads": int,
-        "numMultiMapReads": int,
-        "transcripts": (lambda s: s.split(',') if len(s) > 0 else [],
-                        lambda l: ",".join(l)),
-    }
-
-    def __init__(self, evidTsv):
-        super(GencodeIntronEvidReader, self).__init__(evidTsv, typeMap=GencodeIntronEvidReader.spliceJunctionTsvTypeMap)
-
-
-class GencodeIntronEvidSet(list):
-    """
-    Set of all intron evidence for a release.
-    """
-    def __init__(self, evidTsv):
-        self.novel = []
-        # this mixes transcript loci by PAR, sorted out with get
-        self.byTranscriptId = defaultdict(list)
-        self._loadEvid(evidTsv)
-
-    def _loadEvid(self, evidTsv):
-        for evid in GencodeIntronEvidReader(evidTsv):
-            self._loadEvidRec(evid)
-
-    def _loadEvidRec(self, evid):
-        self.append(evid)
-        if len(evid.transcripts) == 0:
-            self.novel.append(evid)
-        else:
-            for ti in evid.transcripts:
-                self.byTranscriptId[ti].append(evid)
-
-    def getByTranscriptLocus(self, transLocus):
-        "get introns for a transcript locus, sorted in genomic order"
-        evids = self.byTranscriptId[transLocus.transcript.id]
-        if transLocus.hasMultiLoci():
-            # find ones on same chrom
-            evids = [evid for evid in evids if evid.chrom == transLocus.chrom]
-        return tuple(sorted(evids, key=lambda r: r.intronStart))
-
-
 class SpliceJuncCat(SymEnum):
     consensus = 1
     known = 2
